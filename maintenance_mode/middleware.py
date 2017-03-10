dc02be2ee63452ad5f1dd89f6edab9e82a122edf
ezamoragomez <edgar.zamoragomez@therealbuzzgroup.com>
2017-03-10 16:44:39 +0000

    Initial commit. Update gitignore

--- maintenance_mode/middleware.py
+++ maintenance_mode/middleware.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import django
+import re
+import sys
+
 from django.core.urlresolvers import NoReverseMatch, resolve, reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -14,14 +17,11 @@ from django.utils.cache import add_never_cache_headers
 if django.VERSION < (1, 10):
     __MaintenanceModeMiddlewareBaseClass = object
 else:
-    #https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
+    # https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
     from django.utils.deprecation import MiddlewareMixin
     __MaintenanceModeMiddlewareBaseClass = MiddlewareMixin
 
 from maintenance_mode import core, settings, utils
-
-import re
-import sys
 
 
 class MaintenanceModeMiddleware(__MaintenanceModeMiddlewareBaseClass):
@@ -44,18 +44,24 @@ class MaintenanceModeMiddleware(__MaintenanceModeMiddlewareBaseClass):
 
             if hasattr(request, 'user'):
 
-                if settings.MAINTENANCE_MODE_IGNORE_STAFF and request.user.is_staff:
+                if settings.MAINTENANCE_MODE_IGNORE_STAFF \
+                        and request.user.is_staff:
                     return None
 
-                if settings.MAINTENANCE_MODE_IGNORE_SUPERUSER and request.user.is_superuser:
+                if settings.MAINTENANCE_MODE_IGNORE_SUPERUSER \
+                        and request.user.is_superuser:
                     return None
 
             if settings.MAINTENANCE_MODE_IGNORE_TEST:
 
                 is_testing = False
 
-                if (len(sys.argv) > 0 and 'runtests' in sys.argv[0]) or (len(sys.argv) > 1 and sys.argv[1] == 'test'):
-                    #python runtests.py | python manage.py test | python setup.py test | django-admin.py test
+                if (len(sys.argv) > 0 and 'runtests' in sys.argv[0]) \
+                        or (len(sys.argv) > 1 and sys.argv[1] == 'test'):
+                    # python runtests.py |
+                    # python manage.py test |
+                    # python setup.py test |
+                    # django-admin.py test
                     is_testing = True
 
                 if is_testing:
@@ -63,7 +69,8 @@ class MaintenanceModeMiddleware(__MaintenanceModeMiddlewareBaseClass):
 
             if settings.MAINTENANCE_MODE_IGNORE_IP_ADDRESSES:
 
-                for ip_address in settings.MAINTENANCE_MODE_IGNORE_IP_ADDRESSES:
+                for ip_address \
+                        in settings.MAINTENANCE_MODE_IGNORE_IP_ADDRESSES:
 
                     ip_address_re = re.compile(ip_address)
 
@@ -81,12 +88,14 @@ class MaintenanceModeMiddleware(__MaintenanceModeMiddlewareBaseClass):
 
             if settings.MAINTENANCE_MODE_REDIRECT_URL:
 
-                redirect_url_re = re.compile(settings.MAINTENANCE_MODE_REDIRECT_URL)
+                redirect_url_re = re.compile(
+                    settings.MAINTENANCE_MODE_REDIRECT_URL)
 
                 if redirect_url_re.match(request.path_info):
                     return None
 
-                return HttpResponseRedirect(settings.MAINTENANCE_MODE_REDIRECT_URL)
+                return HttpResponseRedirect(
+                    settings.MAINTENANCE_MODE_REDIRECT_URL)
 
             else:
 
@@ -94,18 +103,30 @@ class MaintenanceModeMiddleware(__MaintenanceModeMiddlewareBaseClass):
 
                 if settings.MAINTENANCE_MODE_TEMPLATE_CONTEXT:
 
-                    request_context_func = utils.import_function(settings.MAINTENANCE_MODE_TEMPLATE_CONTEXT)
+                    request_context_func = utils.import_function(
+                        settings.MAINTENANCE_MODE_TEMPLATE_CONTEXT)
 
                     if request_context_func:
-                        request_context = request_context_func(request = request)
+                        request_context = request_context_func(
+                            request=request)
 
                 if django.VERSION < (1, 8):
-                    response = render_to_response(settings.MAINTENANCE_MODE_TEMPLATE, request_context, context_instance=RequestContext(request), content_type='text/html')
+                    response = render_to_response(
+                        settings.MAINTENANCE_MODE_TEMPLATE,
+                        request_context,
+                        context_instance=RequestContext(request),
+                        content_type='text/html'
+                    )
                 else:
-                    response = render(request, settings.MAINTENANCE_MODE_TEMPLATE, context=request_context, content_type='text/html', status=503)
+                    response = render(
+                        request,
+                        settings.MAINTENANCE_MODE_TEMPLATE,
+                        context=request_context,
+                        content_type='text/html',
+                        status=503
+                    )
 
                 add_never_cache_headers(response)
                 return response
         else:
             return None
-
